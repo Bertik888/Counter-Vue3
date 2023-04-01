b865d945f942a46cce0990a3a1eb2430ba2006eb
Bertik888 <alberttuhvatshin@mail.ru>
2023-04-01 23:32:44 +0300

    5 commit with input, not validation max_count

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,13 @@
     <div class="row">
 
       <div class="col">
+        <c-model 
+          @changeCounter="changeCounter"
+          :max_count="max_count"          
+        />
+      </div>
+
+      <div class="col">
         <c-view-1 
           :counter="counter"
           @resetCounter="resetCounter"
@@ -23,19 +30,23 @@
 <script>
 import CView1 from './components/CView1.vue'
 import CAction1 from './components/CAction1.vue'
+import CModel from './components/CModel.vue'
+
 
 
 export default {
   name: 'App',
   components: {
     CView1,
-    CAction1
+    CAction1,
+    CModel
   },
 
   data() {
     return {
       counter: 0,
-      reset_counter: 0 //так можно описать или лучше CamelCase?
+      reset_counter: 0, //так можно описать или лучше CamelCase?
+      max_count: 10 
     }
   },
 
@@ -43,20 +54,36 @@ export default {
     increment() {
       console.log('increment') // не работает почему? [X] - есть ответ
       this.counter++  
-    },
-    
-    resetCounter() {
-      console.log(resetCounter)
-      if (resetCounter > 0) {
+      if (this.counter > this.max_count) {
         this.counter = 0
-        this.reset_counter++
-      } else {
-        console.log('Error:')
+        // this.reset_counter = 0
       }
+    },
 
-    }
-  },
+    resetCounter() {      
+      if (this.counter !== 0) {
+        this.counter = 0 
+        this.reset_counter++
+        console.log('resetCounter')
+      }            
+      if (this.reset_counter > this.max_count) {
+        this.reset_counter = 0
+        // this.counter = 0        
+      }      
+    },
+
+    changeCounter() {
+      /* if (this.counter !== 0 && this.inputValue > 0) {
+        this.counter++
+      } */
+    },
+  }
+
 }
+
+      /* else {
+        console.log('error_valid', val, this.max_count)
+      } */
 </script>
 
 <style lang="scss">
---
+++ src/components/CModel.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="block-wrapper"> 
+
+    <div class="c-model">
+      <div class="my-input">     
+        input counter:    
+        <input 
+          v-model="inputValue" 
+          
+          type="number"
+          class="input"        
+        >
+      </div>
+      <!-- :max_count="max_count" нужен ли тут пропс в инпуте? - [x] - ответ, нет не нужен-->
+        
+      <div class="btn">
+        <button
+          @click="$emit('changeCounter')"          
+          class="button"
+        >
+          change counter
+        </button>
+      </div>
+
+    </div>
+
+  </div>  
+</template>
+
+
+<script>
+  export default {
+    name: 'CModel',
+
+    props: {
+      max_count: {
+        type: Number,
+        default: 10
+      },
+    },
+
+    data() {
+      return {
+        inputValue: 0
+      }
+    },
+
+    methods: {
+      changeCounter() {
+        
+        this.$emit.inputValue
+        // this.inputValue = 0
+
+        if (this.counter !== 0) {
+          this.counter++
+          } else {
+            console.log('error_valid', val, this.max_count)
+        }
+
+        console.log(inputValue)
+
+        console.log('changeCounter')
+      }
+    }
+  }
+</script>
+
+
+
+
+
+<style lang="scss" scoped>
+.c-model {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  align-items: center;
+}
+
+.input {
+  border: 0;
+  outline: 0;
+  background: 0;
+}
+
+  input[type="number"]::-webkit-outer-spin-button,
+  input[type="number"]::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+
+</style>
